5498269a6711cb58120679df268ed75171590b96
Naddybs <144009698+Naddybs@users.noreply.github.com>
2023-12-11 18:50:41 +0100

    5 buttons versie 1

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,3 +1,4 @@
+// button 12
 let interaction = document.querySelector('a:nth-of-type(12)')
 
 interaction.addEventListener('click', jumpHandler)
@@ -6,3 +7,56 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+// button 1
+ let frontend = document.querySelector('a:nth-of-type(1)')
+
+ frontend.addEventListener('click', colorchange)
+ function colorchange(){
+ frontend.classList.toggle('color')
+}
+// button 2
+let design = document.querySelector('a:nth-of-type(2)')
+
+design.addEventListener('click', resizebutton)
+function resizebutton(){
+  design.classList.toggle('resize')
+}
+// button 3
+let en = document.querySelector('a:nth-of-type(3)')
+
+en.addEventListener('click', borderchange)
+function borderchange(){
+  en.classList.toggle('border')
+}
+// button 4
+let development = document.querySelector('a:nth-of-type(4)')
+
+development.addEventListener('click', gradientshow)
+function gradientshow(){
+  development.classList.toggle('gradient')
+}
+// button 5
+let sprint = document.querySelector('a:nth-of-type(5)');
+
+sprint.addEventListener('click', shakeButton);
+
+function shakeButton() {
+  sprint.classList.toggle('shake');
+}
+// button 6
+let fix = document.querySelector('a:nth-of-type(6)');
+
+fix.addEventListener('click', flipButton);
+
+function flipButton() {
+  fix.classList.toggle('flip');
+}
+
+let the = document.querySelector('a:nth-of-type(7)');
+
+the.addEventListener('click', turnButton);
+
+function turnButton() {
+  the.classList.toggle('turn');
+}
+
