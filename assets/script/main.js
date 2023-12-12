30e88dab17b6e93f50f65c6a55e59c1fd92e3f7e
Naddybs <144009698+Naddybs@users.noreply.github.com>
2023-12-13 00:39:18 +0100

    10 buttons fix

--- assets/script/main.js
+++ assets/script/main.js
@@ -8,12 +8,28 @@ function jumpHandler() {
   interaction.classList.toggle('jump')
 }
 // button 1
- let frontend = document.querySelector('a:nth-of-type(1)')
 
+// stap 1: selecteer de button
+ let frontend = document.querySelector('a:nth-of-type(1)')
+//stap 2: voeg een eventListenet toe voor zowel de click- als keydown event
  frontend.addEventListener('click', colorchange)
+ document.addEventListener('keydown', checkKeyPress);
+// stap 3: definieer de functie (colorchange)
  function colorchange(){
- frontend.classList.toggle('color')
+  console.log("Button geklikt :)");
+  //  stap 4: toggle de 'color' class op en af
+ frontend.classList.toggle('color');
 }
+// stap 5: definieer de functie (checkkeypress)
+function checkKeyPress(event) {
+  // stap 6: controleert of de spatiebalk (keycode 32) is ingedrukt
+  if (event.keyCode === 32) {
+    // stap 7: roep de colorchange functie op als je op de spatiebalk drukt
+    colorchange();
+  }
+}
+
+
 // button 2
 let design = document.querySelector('a:nth-of-type(2)')
 
@@ -46,12 +62,12 @@ function shakeButton() {
 // button 6
 let fix = document.querySelector('a:nth-of-type(6)');
 
-fix.addEventListener('click', flipButton);
+fix.addEventListener('mouseover', flipButton);
 
 function flipButton() {
   fix.classList.toggle('flip');
 }
-
+// button 7
 let the = document.querySelector('a:nth-of-type(7)');
 
 the.addEventListener('click', turnButton);
@@ -59,4 +75,33 @@ the.addEventListener('click', turnButton);
 function turnButton() {
   the.classList.toggle('turn');
 }
+// button 8
+let flow = document.querySelector('a:nth-of-type(8)');
+let popup = document.querySelector('.popup');
 
+flow.addEventListener('dblclick', showpopup);
+function showpopup(){
+  popup.classList.toggle('popupshow');
+}
+// button 9
+let user = document.querySelector('a:nth-of-type(9)');
+
+document.addEventListener('keydown', shiftslideright);
+function shiftslideright(event) {
+  if (event.shiftKey && event.target === user){
+    slideright()
+  }
+}
+  function slideright(){
+  user.classList.toggle('slide')}
+
+  // button 10
+  let interface = document.querySelector('a:nth-of-type(10)');
+
+  interface.addEventListener('keydown', discocolors);
+  
+  function discocolors(event) {
+    if (event.key === 'd') {
+      interface.classList.toggle('color-change-animated');
+    }
+  }
